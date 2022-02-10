a1922ac20a95a7b6a84e80d421788f6a0c5c484d
MW-RESURGENT <mwu2021@gmail.com>
2022-02-09 17:06:47 -0800

    Worked on initial version of Hive Software v0.1.

---
+++ Hive Software v0.1/hive_lamp/hive_lamp.ino
@@ -0,0 +1,436 @@
+/*
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
+ * Decawave DW1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file RangingAnchor.ino
+ * Use this to test two-way ranging functionality with two
+ * DW1000. This is the anchor component's code which computes range after
+ * exchanging some messages. Addressing and frame filtering is currently done
+ * in a custom way, as no MAC features are implemented yet.
+ *
+ * Complements the "RangingTag" example sketch.
+ *
+ * @todo
+ *  - weighted average of ranging results based on signal quality
+ *  - use enum instead of define
+ *  - move strings to flash (less RAM consumption)
+ */
+
+#include <SPI.h>
+#include <FastLED.h>
+#include <DW1000.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// I/O PINS
+
+#define PIN_LED 7 // WS2812B LED data pin
+
+const uint8_t PIN_RST = 8; // reset pin
+const uint8_t PIN_IRQ = 9; // irq pin
+const uint8_t PIN_SS = 10; // spi select pin
+
+///////////////////////////////////////////////////////////////////////////////
+// LAMP AND TAG CONSTRUCT VALUES
+
+#define NUM_LEDS 1
+#define CHIPSET WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+struct bulbProps {
+    int fixtureBrightness;
+    int[256] brightnessCurve;
+};
+
+struct TagVars {
+    int rangeOfActivation;
+    byte brightness;
+    byte redChannel;
+    byte greenChannel;
+    byte blueChannel;
+    int turnOnDelay;
+    int shutOffDelay;
+    int fadeInRate;
+    int fadeOutRate;
+    byte fadeOnRangeOnly;
+    byte cutOffMaxBrightness;
+    byte cutOffMinBrightness;
+};
+
+struct TagControl {
+    byte authLevel;
+    byte collisionMode;
+};
+
+enum tagAuthLevels {
+    ENV_MASTER,
+    ADMIN,
+    GENERAL
+};
+
+enum collisionHeuristic {
+    PREEMINENCE,
+    BLENDING,
+    FIRST_COME_FIRST_SERVE
+};
+
+enum LEDModes {
+    BRIGHTER_WHEN_CLOSER,
+    DIMMER_WHEN_CLOSER,
+    CONSTANT_BRIGHTNESS_WITHIN_RANGE,
+    CONSTANT_BRIGHTNESS_OUTSIDE_RANGE,
+    N_CLOSEST_LIGHTS,
+    N_FARTHEST_LIGHTS
+};
+
+byte LEDBrightnessTable[256] = {
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4,
+    4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8,
+    8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12, 12, 12, 13, 13, 14,
+    14, 15, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 22,
+    22, 23, 23, 24, 25, 25, 26, 26, 27, 28, 28, 29, 30, 30, 31, 32,
+    33, 33, 34, 35, 36, 36, 37, 38, 39, 40, 40, 41, 42, 43, 44, 45,
+    46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78,
+    80, 81, 82, 83, 85, 86, 87, 89, 90, 91, 93, 94, 95, 97, 98, 99,
+    101, 102, 104, 105, 107, 108, 110, 111, 113, 114, 116, 117, 119, 121, 122, 124,
+    125, 127, 129, 130, 132, 134, 135, 137, 139, 141, 142, 144, 146, 148, 150, 151,
+    153, 155, 157, 159, 161, 163, 165, 166, 168, 170, 172, 174, 176, 178, 180, 182,
+    184, 186, 189, 191, 193, 195, 197, 199, 201, 204, 206, 208, 210, 212, 215, 217,
+    219, 221, 224, 226, 228, 231, 233, 235, 238, 240, 243, 245, 248, 250, 253, 255};
+
+///////////////////////////////////////////////////////////////////////////////
+// DWM1000 UWB MODULE VARIABLES
+
+#define POLL 0
+#define POLL_ACK 1
+#define RANGE 2
+#define RANGE_REPORT 3
+#define RANGE_FAILED 255
+// message flow state
+volatile byte expectedMsgId = POLL;
+// message sent/received state
+volatile boolean sentAck = false;
+volatile boolean receivedAck = false;
+// protocol error state
+boolean protocolFailed = false; // may be read externally
+// timestamps to remember
+DW1000Time timePollSent;
+DW1000Time timePollReceived;
+DW1000Time timePollAckSent;
+DW1000Time timePollAckReceived;
+DW1000Time timeRangeSent;
+DW1000Time timeRangeReceived;
+// last computed range/time
+DW1000Time timeComputedRange;
+// data buffer
+#define LEN_DATA 16
+byte data[LEN_DATA];
+// watchdog and reset period (in milliseconds)
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+// reply times (same on both sides for symm. ranging)
+uint16_t replyDelayTimeUS = 3000;
+// ranging counter (per second)
+uint16_t successRangingCount = 0;
+uint32_t rangingCountPeriod = 0;
+float samplingRate = 0;
+
+///////////////////////////////////////////////////////////////////////////////
+// DWM1000 UWB COMMUNICATION LOGIC
+
+// update activity timestamp, so that we do not reach "resetPeriod"
+void noteActivity() {
+    lastActivity = millis();
+}
+
+void resetInactive() {
+    // anchor listens for POLL
+    expectedMsgId = POLL;
+    receiver();
+    noteActivity();
+}
+
+// Handler function for when a communication is successfully sent by DWM1000 module.
+void handleSent() {
+    // status change on sent success
+    sentAck = true;
+}
+
+// Handler function for when a communication is successfully received by DWM1000 module.
+void handleReceived() {
+    receivedAck = true;
+}
+
+void transmitPollAck() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = POLL_ACK;
+    // delay the same amount as ranging tag
+    DW1000Time deltaTime = DW1000Time(replyDelayTimeUS, DW1000Time::MICROSECONDS);
+    DW1000.setDelay(deltaTime);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void transmitRangeReport(float curRange) {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE_REPORT;
+    // write final ranging result
+    memcpy(data + 1, &curRange, 4);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void transmitRangeFailed() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE_FAILED;
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void receiver() {
+    DW1000.newReceive();
+    DW1000.setDefaults();
+    // so we don't need to restart the receiver manually
+    DW1000.receivePermanently(true);
+    DW1000.startReceive();
+//    Serial.println(F("Receiver Routine Started"));
+}
+
+void initializeUWBModule() {
+    // initialize ultra-wideband hardware (DWM1000)
+    Serial.println(F("### DW1000-arduino-ranging-anchor ###"));
+    // initialize the driver
+    DW1000.begin(PIN_IRQ, PIN_RST);
+    DW1000.select(PIN_SS);
+    Serial.println(F("DW1000 initialized ..."));
+    // general configuration
+    DW1000.newConfiguration();
+    DW1000.setDefaults();
+    DW1000.setDeviceAddress(1);
+    DW1000.setNetworkId(10);
+    DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    DW1000.commitConfiguration();
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000.getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000.getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000.getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000.getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);
+    // attach callback for (successfully) sent and received messages
+    DW1000.attachSentHandler(handleSent);
+    DW1000.attachReceivedHandler(handleReceived);
+    // anchor starts in receiving mode, awaiting a ranging poll message
+    receiver();
+    noteActivity();
+    // for first time ranging frequency computation
+    rangingCountPeriod = millis();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+ * RANGING ALGORITHMS
+ * ------------------
+ * Either of the below functions can be used for range computation (see line "CHOSEN
+ * RANGING ALGORITHM" in the code).
+ * - Asymmetric is more computation intense but least error prone
+ * - Symmetric is less computation intense but more error prone to clock drifts
+ *
+ * The anchors and tags of this reference example use the same reply delay times, hence
+ * are capable of symmetric ranging (and of asymmetric ranging anyway).
+ */
+
+void computeRangeAsymmetric() {
+    // asymmetric two-way ranging (more computation intense, less error prone)
+    DW1000Time round1 = (timePollAckReceived - timePollSent).wrap();
+    DW1000Time reply1 = (timePollAckSent - timePollReceived).wrap();
+    DW1000Time round2 = (timeRangeReceived - timePollAckSent).wrap();
+    DW1000Time reply2 = (timeRangeSent - timePollAckReceived).wrap();
+    DW1000Time tof = (round1 * round2 - reply1 * reply2) / (round1 + round2 + reply1 + reply2);
+    // set tof timestamp
+    timeComputedRange.setTimestamp(tof);
+}
+
+void computeRangeSymmetric() {
+    // symmetric two-way ranging (less computation intense, more error prone on clock drift)
+    DW1000Time tof = ((timePollAckReceived - timePollSent) - (timePollAckSent - timePollReceived) +
+                      (timeRangeReceived - timePollAckSent) - (timeRangeSent - timePollAckReceived)) * 0.25f;
+    // set tof timestamp
+    timeComputedRange.setTimestamp(tof);
+}
+
+/*
+ * END RANGING ALGORITHMS
+ * ----------------------
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+// What kind of behavioral mode do we want the lights to be in?
+// VARIABLES:
+// - Range of activation                 - meters or centimeters
+// - Brightness (of each color channel)  - percent
+// - Time delay for shutoff              - seconds or milliseconds
+// - Time delay for turn on              - seconds or milliseconds
+// - Fade-in rate                        - seconds or milliseconds
+// - Fade-out rate                       - seconds or milliseconds
+// - Fade on range only                  - boolean
+// - Set color                           - set list of colors or RGB plot
+// - Specific brightness of each fixture - lumens
+// - Cutoff max brightness               - percent or lumens
+// - Cutoff min brightness               - percent or lumens
+//
+// MODES:
+// 1 - Brighter when closer
+// 2 - Dimmer when closer
+// 3 - Constant brightness within range threshold
+// 4 - Constant brightness outside of range threshold
+// 5 - X closest lights
+// 6 - X farthest lights
+
+void initializeLEDMode() {
+    FastLED.addLeds<CHIPSET, PIN_LED, COLOR_ORDER>(leds, NUM_LEDS);
+}
+
+void toggleLEDMode(int ledModes, int setTo) {
+//    switch(ledMode) {
+//        case -1:
+//          if 
+//    }
+}
+
+// How do we determine how the light changes in correspondence to its defined
+// behavior?
+int calculateLEDChange() {
+    
+}
+
+// In acknowledgement of the character of the hardware (LEDs) we are using,
+// what do we need to keep in mind?
+//
+// WS2812B brightness curves
+
+
+///////////////////////////////////////////////////////////////////////////////
+// SETUP EXECUTION ROUTINE
+
+void initializeSerialOutput() {
+    Serial.begin(115200);
+    delay(8000);
+}
+
+void setup() {
+    initializeSerialOutput();
+    initializeLEDMode();
+    initializeUWBModule();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// LOOPING EXECUTION ROUTINE
+
+void loop() {
+    int32_t curMillis = millis();
+
+    // Appears to be for if there is no activity and the hardware times out,
+    // then reset it so it (receiver mechanism) will listen for polling requests again.
+    if (!sentAck && !receivedAck) {
+        // check if inactive
+        if (curMillis - lastActivity > resetPeriod) {
+            resetInactive();
+        }
+        return;
+    }
+//    Serial.print(sentAck);
+//    Serial.print(" ");
+//    Serial.print(receivedAck);
+//    Serial.println();
+    
+    // If message sent successfully, then get DW1000 precision timestamp
+    // for when it was sent.
+    if (sentAck) {
+        sentAck = false;
+        byte msgId = data[0];
+        if (msgId == POLL_ACK) {
+            DW1000.getTransmitTimestamp(timePollAckSent);
+            noteActivity();
+        }
+    }
+
+    // Upon receipt of message, 
+    if (receivedAck) {
+        receivedAck = false;
+        // get incoming message and parse
+        DW1000.getData(data, LEN_DATA);
+        Serial.print(" ");
+        Serial.println(data[0]);
+        byte msgId = data[0];
+        if (msgId != expectedMsgId) { //
+            // unexpected message, start over again (except if already POLL)
+            protocolFailed = true;
+        }
+        if (msgId == POLL) {
+            // on POLL we (re-)start, so no protocol failure
+            protocolFailed = false;
+            DW1000.getReceiveTimestamp(timePollReceived);
+            expectedMsgId = RANGE;
+            transmitPollAck();
+            noteActivity();
+        }
+        else if (msgId == RANGE) {
+            DW1000.getReceiveTimestamp(timeRangeReceived);
+            expectedMsgId = POLL;
+            if (!protocolFailed) {
+                timePollSent.setTimestamp(data + 1);
+                timePollAckReceived.setTimestamp(data + 6);
+                timeRangeSent.setTimestamp(data + 11);
+                // (re-)compute range as two-way ranging is done
+                computeRangeAsymmetric(); // CHOSEN RANGING ALGORITHM
+                transmitRangeReport(timeComputedRange.getAsMicroSeconds());
+                float distance = timeComputedRange.getAsMeters();
+                Serial.print("Range: "); Serial.print(distance); Serial.print(" m");
+                leds[0] = CHSV(distance * 100, distance * 100, distance * 100); FastLED.show(); delay(30);
+//                leds[0] = CRGB::Black; FastLED.show(); delay(30);
+                Serial.print("\t RX power: "); Serial.print(DW1000.getReceivePower()); Serial.print(" dBm");
+                Serial.print("\t Sampling: "); Serial.print(samplingRate); Serial.println(" Hz");
+                //Serial.print("FP power is [dBm]: "); Serial.print(DW1000.getFirstPathPower());
+                //Serial.print("RX power is [dBm]: "); Serial.println(DW1000.getReceivePower());
+                //Serial.print("Receive quality: "); Serial.println(DW1000.getReceiveQuality());
+                // update sampling rate (each second)
+                successRangingCount++;
+                if (curMillis - rangingCountPeriod > 1000) {
+                    samplingRate = (1000.0f * successRangingCount) / (curMillis - rangingCountPeriod);
+                    rangingCountPeriod = curMillis;
+                    successRangingCount = 0;
+                }
+            }
+            else {
+                transmitRangeFailed();
+            }
+
+            noteActivity();
+        }
+    }
+}
---
+++ Hive Software v0.1/hive_tag/hive_tag.ino
@@ -0,0 +1,293 @@
+/*
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
+ * Decawave DW1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file RangingTag.ino
+ * Use this to test two-way ranging functionality with two DW1000. This is
+ * the tag component's code which polls for range computation. Addressing and
+ * frame filtering is currently done in a custom way, as no MAC features are
+ * implemented yet.
+ *
+ * Complements the "RangingAnchor" example sketch.
+ *
+ * @todo
+ *  - use enum instead of define
+ *  - move strings to flash (less RAM consumption)
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include <SPI.h>
+//#include <FastLED.h>
+#include <DW1000.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// I/O PINS
+
+#define PIN_LED 7; // WS2812B LED data pin
+
+const uint8_t PIN_RST = 8; // reset pin
+const uint8_t PIN_IRQ = 9; // irq pin
+const uint8_t PIN_SS = 10; // spi select pin
+
+///////////////////////////////////////////////////////////////////////////////
+// LAMP AND TAG CONSTRUCT VALUES
+
+#define NUM_LEDS 1
+#define CHIPSET WS2812B
+#define COLOR_ORDER GRB
+
+struct TagVars {
+    int rangeOfActivation;
+    byte brightness;
+    byte redChannel;
+    byte greenChannel;
+    byte blueChannel;
+    int turnOnDelay;
+    int shutOffDelay;
+    int fadeInRate;
+    int fadeOutRate;
+    byte fadeOnRangeOnly;
+    byte cutOffMaxBrightness;
+    byte cutOffMinBrightness;
+};
+
+struct TagControl {
+    byte authLevel;
+    byte collisionMode;
+};
+
+enum tagAuthLevels {
+    ENV_MASTER,
+    ADMIN,
+    GENERAL
+};
+
+enum collisionHeuristic {
+    PREEMINENCE,
+    BLENDING,
+    FIRST_COME_FIRST_SERVE
+};
+
+enum LEDModes {
+    BRIGHTER_WHEN_CLOSER,
+    DIMMER_WHEN_CLOSER,
+    CONSTANT_BRIGHTNESS_WITHIN_RANGE,
+    CONSTANT_BRIGHTNESS_OUTSIDE_RANGE,
+    N_CLOSEST_LIGHTS,
+    N_FARTHEST_LIGHTS
+};
+
+byte LEDBrightnessTable[256] = {
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4,
+    4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8,
+    8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 12, 12, 12, 13, 13, 14,
+    14, 15, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 22,
+    22, 23, 23, 24, 25, 25, 26, 26, 27, 28, 28, 29, 30, 30, 31, 32,
+    33, 33, 34, 35, 36, 36, 37, 38, 39, 40, 40, 41, 42, 43, 44, 45,
+    46, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78,
+    80, 81, 82, 83, 85, 86, 87, 89, 90, 91, 93, 94, 95, 97, 98, 99,
+    101, 102, 104, 105, 107, 108, 110, 111, 113, 114, 116, 117, 119, 121, 122, 124,
+    125, 127, 129, 130, 132, 134, 135, 137, 139, 141, 142, 144, 146, 148, 150, 151,
+    153, 155, 157, 159, 161, 163, 165, 166, 168, 170, 172, 174, 176, 178, 180, 182,
+    184, 186, 189, 191, 193, 195, 197, 199, 201, 204, 206, 208, 210, 212, 215, 217,
+    219, 221, 224, 226, 228, 231, 233, 235, 238, 240, 243, 245, 248, 250, 253, 255};
+
+///////////////////////////////////////////////////////////////////////////////
+// DWM1000 UWB MODULE VARIABLES
+
+#define POLL 0
+#define POLL_ACK 1
+#define RANGE 2
+#define RANGE_REPORT 3
+#define RANGE_FAILED 255
+// message flow state
+volatile byte expectedMsgId = POLL_ACK;
+// message sent/received state
+volatile boolean sentAck = false;
+volatile boolean receivedAck = false;
+// timestamps to remember
+DW1000Time timePollSent;
+DW1000Time timePollAckReceived;
+DW1000Time timeRangeSent;
+// data buffer
+#define LEN_DATA 16
+byte data[LEN_DATA];
+// watchdog and reset period
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+// reply times (same on both sides for symm. ranging)
+uint16_t replyDelayTimeUS = 3000;
+
+///////////////////////////////////////////////////////////////////////////////
+// DWM1000 UWB COMMUNICATION LOGIC
+
+// update activity timestamp, so that we do not reach "resetPeriod"
+void noteActivity() {
+    lastActivity = millis();
+}
+
+void resetInactive() {
+    // tag sends POLL and listens for POLL_ACK
+    expectedMsgId = POLL_ACK;
+    transmitPoll();
+    noteActivity();
+}
+
+void handleSent() {
+    // status change on sent success
+    sentAck = true;
+}
+
+void handleReceived() {
+    // status change on received success
+    receivedAck = true;
+}
+
+void transmitPoll() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = POLL;
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+//    Serial.println(F("Transmit Routine Started"));
+}
+
+void transmitRange() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE;
+    // delay sending the message and remember expected future sent timestamp
+    DW1000Time deltaTime = DW1000Time(replyDelayTimeUS, DW1000Time::MICROSECONDS);
+    timeRangeSent = DW1000.setDelay(deltaTime);
+    timePollSent.getTimestamp(data + 1);
+    timePollAckReceived.getTimestamp(data + 6);
+    timeRangeSent.getTimestamp(data + 11);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+    Serial.print("Expect RANGE to be sent @ "); Serial.println(timeRangeSent.getAsFloat());
+}
+
+void receiver() {
+    DW1000.newReceive();
+    DW1000.setDefaults();
+    // so we don't need to restart the receiver manually
+    DW1000.receivePermanently(true);
+    DW1000.startReceive();
+//    Serial.println(F("Receiver Routine Started"));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// SETUP EXECUTION ROUTINE
+
+void initializeSerialOutput() {
+    Serial.begin(115200);
+    delay(8000);
+}
+
+void initializeUWBModule() {
+    Serial.println(F("### DW1000-arduino-ranging-tag ###"));
+    // initialize the driver
+    DW1000.begin(PIN_IRQ, PIN_RST);
+    DW1000.select(PIN_SS);
+    Serial.println("DW1000 initialized ...");
+    // general configuration
+    DW1000.newConfiguration();
+    DW1000.setDefaults();
+    DW1000.setDeviceAddress(2);
+    DW1000.setNetworkId(10);
+    DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    DW1000.commitConfiguration();
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000.getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000.getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000.getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000.getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);
+    // attach callback for (successfully) sent and received messages
+    DW1000.attachSentHandler(handleSent);
+    DW1000.attachReceivedHandler(handleReceived);
+    // anchor starts by transmitting a POLL message
+    receiver();
+    transmitPoll();
+    noteActivity();
+}
+
+void setup() {
+    initializeSerialOutput();
+    initializeUWBModule();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// LOOPING EXECUTION ROUTINE
+
+void loop() {
+    if (!sentAck && !receivedAck) {
+        // check if inactive
+        if (millis() - lastActivity > resetPeriod) {
+            resetInactive();
+        }
+        return;
+    }
+    // continue on any success confirmation
+    if (sentAck) {
+        sentAck = false;
+        byte msgId = data[0];
+        if (msgId == POLL) {
+            DW1000.getTransmitTimestamp(timePollSent);
+            Serial.print("Sent POLL @ "); Serial.println(timePollSent.getAsFloat());
+        } else if (msgId == RANGE) {
+            DW1000.getTransmitTimestamp(timeRangeSent);
+            noteActivity();
+        }
+    }
+    if (receivedAck) {
+        receivedAck = false;
+        // get message and parse
+        DW1000.getData(data, LEN_DATA);
+        byte msgId = data[0];
+        if (msgId != expectedMsgId) {
+            // unexpected message, start over again
+            Serial.print("Received wrong message # "); Serial.println(msgId);
+            expectedMsgId = POLL_ACK;
+            transmitPoll();
+            return;
+        }
+        if (msgId == POLL_ACK) {
+            DW1000.getReceiveTimestamp(timePollAckReceived);
+            expectedMsgId = RANGE_REPORT;
+            transmitRange();
+            noteActivity();
+        } else if (msgId == RANGE_REPORT) {
+            expectedMsgId = POLL_ACK;
+            float curRange;
+            memcpy(&curRange, data + 1, 4);
+            transmitPoll();
+            noteActivity();
+        } else if (msgId == RANGE_FAILED) {
+            expectedMsgId = POLL_ACK;
+            transmitPoll();
+            noteActivity();
+        }
+    }
+}
